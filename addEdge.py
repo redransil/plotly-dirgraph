7f488fc6be183e955274746b750219295619a28a
Abhinav <abhinavkumar.sieger@gmail.com>
2020-07-09 06:17:19 +0530

    Handle the case when y1=y0 or x1=x0, which result in divide by zero in current status.

--- addEdge.py
+++ addEdge.py
@@ -16,18 +16,18 @@ import math
 # arrowAngle is the angle in degrees that the arrowhead makes with the edge
 # dotSize is the plotly scatter dot size you are using (used to even out line spacing when you have a mix of edge lengths)
 def addEdge(start, end, edge_x, edge_y, lengthFrac=1, arrowPos = None, arrowLength=0.025, arrowAngle = 30, dotSize=20):
-    
+
     # Get start and end cartesian coordinates
     x0, y0 = start
     x1, y1 = end
-    
+
     # Incorporate the fraction of this segment covered by a dot into total reduction
     length = math.sqrt( (x1-x0)**2 + (y1-y0)**2 )
     dotSizeConversion = .0565/20 # length units per dot size
     convertedDotDiameter = dotSize * dotSizeConversion
     lengthFracReduction = convertedDotDiameter / length
     lengthFrac = lengthFrac - lengthFracReduction
-    
+
     # If the line segment should not cover the entire distance, get actual start and end coords
     skipX = (x1-x0)*(1-lengthFrac)
     skipY = (y1-y0)*(1-lengthFrac)
@@ -35,7 +35,7 @@ def addEdge(start, end, edge_x, edge_y, lengthFrac=1, arrowPos = None, arrowLeng
     x1 = x1 - skipX/2
     y0 = y0 + skipY/2
     y1 = y1 - skipY/2
-    
+
     # Append line corresponding to the edge
     edge_x.append(x0)
     edge_x.append(x1)
@@ -43,42 +43,43 @@ def addEdge(start, end, edge_x, edge_y, lengthFrac=1, arrowPos = None, arrowLeng
     edge_y.append(y0)
     edge_y.append(y1)
     edge_y.append(None)
-    
+
     # Draw arrow
     if not arrowPos == None:
-        
+
         # Find the point of the arrow; assume is at end unless told middle
         pointx = x1
         pointy = y1
-        eta = math.degrees(math.atan((x1-x0)/(y1-y0)))
-        
+
+        eta = math.degrees(math.atan((x1-x0)/(y1-y0))) if y1!=y0 else 90.0
+
         if arrowPos == 'middle' or arrowPos == 'mid':
             pointx = x0 + (x1-x0)/2
             pointy = y0 + (y1-y0)/2
-            
+
         # Find the directions the arrows are pointing
-        signx = (x1-x0)/abs(x1-x0)
-        signy = (y1-y0)/abs(y1-y0)
-        
+        signx = (x1-x0)/abs(x1-x0) if x1!=x0 else +1    #verify this once
+        signy = (y1-y0)/abs(y1-y0) if y1!=y0 else +1    #verified
+
         # Append first arrowhead
         dx = arrowLength * math.sin(math.radians(eta + arrowAngle))
-        dy = arrowLength * math.cos(math.radians(eta + arrowAngle))    
+        dy = arrowLength * math.cos(math.radians(eta + arrowAngle))
         edge_x.append(pointx)
         edge_x.append(pointx - signx**2 * signy * dx)
         edge_x.append(None)
         edge_y.append(pointy)
         edge_y.append(pointy - signx**2 * signy * dy)
         edge_y.append(None)
-        
+
         # And second arrowhead
         dx = arrowLength * math.sin(math.radians(eta - arrowAngle))
-        dy = arrowLength * math.cos(math.radians(eta - arrowAngle))    
+        dy = arrowLength * math.cos(math.radians(eta - arrowAngle))
         edge_x.append(pointx)
         edge_x.append(pointx - signx**2 * signy * dx)
         edge_x.append(None)
         edge_y.append(pointy)
         edge_y.append(pointy - signx**2 * signy * dy)
         edge_y.append(None)
-    
-    
+
+
     return edge_x, edge_y
